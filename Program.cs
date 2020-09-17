9b2f98722fd3058d34cd61f957bcbd4c05c909ee
Steve Lunnay <steve.lunnay@sapowernetworks.com.au>
2020-09-17 09:50:20 +0930

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
+
+namespace BlobSasGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static string GetBlobSasUri(BlobContainerClient container,
+            string blobName, StorageSharedKeyCredential key, string storedPolicyName = null)
+        {
+            // Create a SAS token that's valid for one hour.
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = container.Name,
+                BlobName = blobName,
+                Resource = "b",
+            };
+
+            if (storedPolicyName == null)
+            {
+                sasBuilder.StartsOn = DateTimeOffset.UtcNow;
+                sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
+                sasBuilder.SetPermissions(BlobContainerSasPermissions.Read);
+            }
+            else
+            {
+                sasBuilder.Identifier = storedPolicyName;
+            }
+
+            // Use the key to get the SAS token.
+            string sasToken = sasBuilder.ToSasQueryParameters(key).ToString();
+
+            Console.WriteLine("SAS for blob is: {0}", sasToken);
+            Console.WriteLine();
+
+            return container.GetBlockBlobClient(blobName).Uri + sasToken;
+        }
+    }
+}
